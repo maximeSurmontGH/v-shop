30c4e2921236a9cef55ef9f29dffcfb0611e4b3d
Maxime Surmont <maxime.surmont@hei.yncrea.fr>
2025-01-21 16:21:39 +0100

    🚀 add stock info

--- app/ui/ItemRow.tsx
+++ app/ui/ItemRow.tsx
@@ -7,20 +7,30 @@ import * as React from "react";
 export interface ItemRowProps {
   name: string;
   price: number;
+  stock: number;
 }
 
-const ItemRow: React.FC<ItemRowProps> = ({ name, price }) => {
+const ItemRow: React.FC<ItemRowProps> = ({ name, price, stock }) => {
   const buy = () => {
     console.log("Not implemented.");
   };
 
   return (
     <div className="flex w-full flex-row items-center justify-between rounded-full bg-white p-1">
-      <span className="mx-5 text-slate-800">{name}</span>
+      <span className="mx-5 text-slate-800">
+        {name}
+
+        {stock > 0 ? (
+          <span className="ml-1 text-sm text-slate-400">({stock})</span>
+        ) : (
+          <span className="ml-1 text-sm text-red-400">(Épuisé)</span>
+        )}
+      </span>
 
       <button
         onClick={buy}
-        className="rounded-full bg-slate-800 p-2 text-white"
+        className={`rounded-full bg-slate-800 p-2 text-white ${stock === 0 ? "cursor-not-allowed opacity-50" : ""}`}
+        disabled={stock === 0}
       >
         <span className="ml-5 flex flex-row items-center">
           <span className="mr-1">{price}</span>
